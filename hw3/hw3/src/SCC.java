19aa22fcdd63d9caf721fd167e6c50e2c2362192
chang02 <chang0022@snu.ac.kr>
2018-05-29 12:17:43 +0900

    hw3

---
+++ hw3/hw3/src/SCC.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class SCC {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		int number = Integer.parseInt(reader.readLine());
+		ArrayList<String> g = new ArrayList<String>();
+		for(int i=0;i<number;i++) {
+			g.add(reader.readLine());
+		}
+		
+		
+	}
+}
